0578cf939f7cd0bc7a7373bcbeb0a965154c8b0d
Othmane Bounri <obounri@e1r4p15.1337.ma>
2022-03-01 17:45:22 +0100

    day01/ done

--- day01/ex01/Zombie.hpp
+++ day01/ex01/Zombie.hpp
@@ -6,7 +6,7 @@
 /*   By: obounri <obounri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/26 15:20:44 by obounri           #+#    #+#             */
-/*   Updated: 2022/03/01 17:40:33 by obounri          ###   ########.fr       */
+/*   Updated: 2022/03/01 17:45:18 by obounri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,4 @@ public:
 };
 
 Zombie* zombieHorde( int N, std::string name );
-
 
